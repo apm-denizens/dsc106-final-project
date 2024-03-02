5f3e2b16b5a9579bc911c267c8debd09ec0c85d8
0eti <samuelzhang012@gmail.com>
2024-03-01 18:54:08 -0800

    writeup added to prototype page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -201,8 +201,15 @@
 
 <div>
 <h1>PCA - Interactive Visualization Prototype</h1>
-<p>move mouse over chart to see projected data</p>
+<p>move mouse over chart to see projected data. prototype writeup below. </p>
 <!-- black border to visualization 1 -->
 <svg id="full" style="border: 1px solid black; border-radius: 5px;"></svg>
+<p>
+    Our project focuses on explaining principal component analysis. A visualization involving a 2d case with data points projected onto the mouse location dictated "unit vector". Each of the smaller red lines that stretch from each data point to the main line are the resulting projections. Those lines correspond to reconstruction error, and the user can move the mouse around to build an intuition for what case minimizes said error. 
+</p>
+<p>
+    Meshing the narrative components with the user-interactive elements will probably be fraught with bugs. There will have to be a lot of nuanced event handling for these cases, and integrating with existing libraries for something like scrolly-telling or properly timing animations, will likely take a lot of trial and error. There won't just be a single interactive visualization, but instead multiple of them that will all share a common state, and the narrative elements can't get in the way of that. 
+
+</p>
 <svg id="projected"></svg>
 </div>
